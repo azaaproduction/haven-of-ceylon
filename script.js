9e9c1008a9f61f4b7ed35d339da22264ad475f82
azaaproduction <azaaproduction@gmail.com>
2026-07-08 04:14:49 +0530

    Add menu toggle functionality to script.js

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Website Ready");
 
 });
+
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+toggle.addEventListener("click", function () {
+    menu.classList.toggle("active");
+});
